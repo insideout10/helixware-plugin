483a474a3fabfb48f8dea7e489c7cc814814e509
David Riccitelli <david@insideout.io>
2014-06-16 20:11:58 +0300

    prepare for unit tests

---
+++ tests/test_admin_settings.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Test the *admin/settings* functions.
+ */
+
+class AdminSettingsTest extends WP_UnitTestCase
+{
+
+    /**
+     * Set up the test.
+     */
+    function setUp()
+    {
+        parent::setUp();
+    }
+
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    public function test() {
+
+        // TODO: write tests.
+
+    }
+
+}
