ce323a6523b14a30ce35d66c8a566a4be8791ce7
pieroit <savastanos@libero.it>
2014-07-08 19:24:19 +0200

    add volume persistence via localStorage/cookies

--- src/js/helixware.js
+++ src/js/helixware.js
@@ -3,4 +3,14 @@ jQuery(function ($) {
     // Telling videojs where the flash fallback is.
     videojs.options.flash.swf = videojs_params.swfurl;
 
+    // Looping over players
+    $('.hewa-player').each(function( i, el ){
+
+    	var id = $(el).attr('id');
+
+	    // Adding persistvolume plugin
+	    var vid = videojs( id );
+	    vid.persistvolume({namespace: 'So-Viral-So-Hot'});
+	});
+
 });
