cbd03c44d8e121f2d7a0f1b3d98b63fd8532cb80
David Riccitelli <david@insideout.io>
2015-09-30 18:25:06 +0300

    see #10: move asset related functions to the asset service

--- src/includes/class-helixware-asset-service.php
+++ src/includes/class-helixware-asset-service.php
@@ -61,4 +61,68 @@ class HelixWare_Asset_Service {
 
 	}
 
+	/**
+	 * Set the last modified date.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param int $id The attachment id.
+	 * @param string $value The last modified date value.
+	 */
+	public function set_last_modified_date( $id, $value ) {
+
+		$this->_update_post_meta( $id, HelixWare_Asset_Service::META_LAST_MODIFIED_DATE, $value );
+
+	}
+
+	/**
+	 * Set the type.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param int $id The attachment id.
+	 * @param string $value The type (OnDemand, Live, Broadcast, Channel).
+	 */
+	public function set_type( $id, $value ) {
+
+		// Save the type (Live, OnDemand, Broadcast, Channel).
+		$this->_update_post_meta( $id, HelixWare_Asset_Service::META_TYPE, $value );
+
+	}
+
+	/**
+	 * Set the thumbnail URL. The default width is 230 which is 130 (the WP Media Library tile width) / 9 * 16 (inverse of 16:9).
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param int $id The attachment id.
+	 * @param string $value The thumbnail URL.
+	 * @param int $width The requested width (if not provided, it'll be set to 230 by default).
+	 */
+	public function set_thumbnail_url( $id, $value = NULL, $width = 230 ) {
+
+		// Save a reference to the thumbnail if it exists.
+		$this->_update_post_meta( $id, HelixWare_Asset_Service::META_THUMBNAIL_URL, ( isset( $value ) ? "$value?width=$width" : NULL ) );
+
+	}
+
+	/**
+	 * Convenience method called by the set methods.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param int $id The post id.
+	 * @param string $key The key name.
+	 * @param string|int|null $value The value to set for the key. If the value is NULL, the key will be removed.
+	 */
+	private function _update_post_meta( $id, $key, $value = NULL ) {
+
+		if ( isset( $value ) ) {
+			update_post_meta( $id, $key, $value );
+		} else {
+			delete_post_meta( $id, $key );
+		}
+
+	}
+
 }
