349eefe1d79002173bdc912e217ec25d1bc5fc34
David Riccitelli <david@insideout.io>
2015-10-06 18:39:26 +0300

    see #20: add skeleton for MICO extensions support

---
+++ src/helixware_mico.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ **
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://insideout.io
+ * @since             1.0.0
+ * @package           Helixware_Mico
+ *
+ * @wordpress-plugin
+ * Plugin Name:       HelixWare MICO Extensions
+ * Plugin URI:        http://helixware.tv
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            David Riccitelli
+ * Author URI:        http://insideout.io
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       helixware-mico
+ * Domain Path:       /languages
+ */
+
+require_once( 'helixware-mico/helixware-mico.php' );
