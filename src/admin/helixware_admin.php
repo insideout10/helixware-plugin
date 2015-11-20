4109ef0e33b6670a19c23a886bcff9b39e078fbf
David Riccitelli <david@insideout.io>
2015-11-20 17:25:25 +0200

    see #37: do not load the upload scripts in the whole admin backend but only when on the media new page

--- src/admin/helixware_admin.php
+++ src/admin/helixware_admin.php
@@ -6,53 +6,6 @@
 // Add support for the *live asset* ajax method.
 require_once( 'ajax/helixware_admin_ajax_live_assets.php' );
 
-
-/**
- * Load scripts for the admin screen.
- */
-function hewa_admin_scripts() {
-
-	// Get the configuration options.
-	$options = array(
-		'url'           => HELIXWARE_CLIENT_URL . '/4/user/ondemand',
-		'key'           => hewa_get_option( HEWA_SETTINGS_APPLICATION_KEY ),
-		'secret'        => hewa_get_option( HEWA_SETTINGS_APPLICATION_SECRET ),
-		'extensions'    => hewa_get_option( HEWA_SETTINGS_FILE_EXTENSIONS ),
-		'form_action'   => admin_url( 'admin-ajax.php' ),
-		'ajax_action'   => 'hewa_create_post',
-		'max_file_size' => HEWA_SETTINGS_MAX_FILE_SIZE,
-		'post_types'    => array_map(
-			function ( $item ) {
-				return array(
-					'title' => $item->labels->singular_name
-				);
-			},
-			get_post_types( array( 'public' => TRUE ), 'objects' )
-		),
-		'labels'        => array(
-			'title' => __( 'Title', HEWA_LANGUAGE_DOMAIN ),
-			'tags'  => __( 'Tags', HEWA_LANGUAGE_DOMAIN ),
-			'save'  => __( 'Save', HEWA_LANGUAGE_DOMAIN )
-		),
-	);
-
-	// Enqueue the admin script.
-	wp_enqueue_script( 'hewa_admin_js', dirname( plugin_dir_url( __FILE__ ) ) . '/js/helixware.admin.js', array(
-		'wp-plupload',
-		'plupload-handlers'
-	), FALSE, TRUE );
-
-	// Set the options.
-	wp_localize_script( 'hewa_admin_js', 'hewa_admin_options', $options );
-
-	// Enqueue the stylesheet.
-	wp_enqueue_style( 'hewa_admin_css', plugins_url( 'css/helixware.admin.css', __FILE__ ) );
-
-}
-
-add_action( 'admin_enqueue_scripts', 'hewa_admin_scripts' );
-
-
 /**
  * Add the maximum upload file for HelixWare uploads.
  */
